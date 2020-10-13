a04c159f349d3523eafc1a7c615bc8a011d92cb6
Julien-V <julien.vassort61@gmail.com>
2020-10-13 18:16:41 +0200

    Add Travis

---
+++ pur_beurre/travis.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# coding : utf-8
+
+from . import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': '',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    }
+}
